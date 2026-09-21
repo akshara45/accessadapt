f17b4ea8aa2d4f1cc078c21112138a0d08da6d42
anunandakanakarajan <anunandakanakarajan@gmail.com>
2026-09-21 11:00:07 -0700

    Update main.jsx

--- src/pages/Popup/main.jsx
+++ src/pages/Popup/main.jsx
@@ -236,6 +236,25 @@ function Popup() {
               <strong>{suggestion.title}</strong>
 
               <p>{suggestion.reason}</p>
+              {!suggestion.informational && (
+  <button
+    className="button button-primary"
+    type="button"
+    onClick={() => {
+      chrome.tabs.query(
+        { active: true, currentWindow: true },
+        ([tab]) => {
+          chrome.tabs.sendMessage(tab.id, {
+            type: 'APPLY_ADAPTATION',
+            adaptationType: suggestion.id,
+          });
+        }
+      );
+    }}
+  >
+    Apply
+  </button>
+)}
             </div>
           ))}
         </section>
